1f3fdd6c77d50544f7659389194d2176e5c5151e
Prathm9230 <133800729+Prathm9230@users.noreply.github.com>
2023-06-17 11:01:11 +0530

    Update Defaultvalue.java

--- src/Defaultvalue.java
+++ src/Defaultvalue.java
@@ -1,22 +1,22 @@
 public class Defaultvalue {
     public static boolean a;
     public static byte defByte;
-    public static short Short;
-    public static int Int;
-    public static long Long;
-    public static float Float;
-    public static double Double;
-    public static char Char;
+    public static short defShort;
+    public static int defInt;
+    public static long defLong;
+    public static float defFloat;
+    public static double defDouble;
+    public static char defChar;
     public static void main(String[] args)
     {
         System.out.println(" -- Default values of primitive data types are --:");
         System.out.println("Default boolean value is:"+ a);
         System.out.println("Default byte value is:"+ defByte);
-        System.out.println("Default short value is:"+ Short);
-        System.out.println("Default int value is:"+ Int);
-        System.out.println("Default long value is:"+ Long);
-        System.out.println("Default float value is:"+ Float);
-        System.out.println("Default double value is:"+ Double);
-        System.out.println("Default char value is:"+ Char);
+        System.out.println("Default short value is:"+defShort);
+        System.out.println("Default int value is:"+ defInt);
+        System.out.println("Default long value is:"+ defLong);
+        System.out.println("Default float value is:"+ defFloat);
+        System.out.println("Default double value is:"+ defDouble);
+        System.out.println("Default char value is:"+ defChar);
     }
 }
