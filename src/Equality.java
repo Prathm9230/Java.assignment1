53cdff7d4d1d1a1512b51c8eb58a062b1c2d7bf2
Prathm <jawahireprathmesh@gmail.com>
2023-06-17 10:06:19 +0530

    Basic java programs done

---
+++ src/Equality.java
@@ -0,0 +1,15 @@
+public class Equality {
+    public static void main(String[] args)
+    {
+        String str1 = "Hello! This is Prathmesh.";
+        String str2="Hello! this is Prathmesh.";
+        if(str1 == str2)
+        {
+            System.out.println("str1 is equals to str2");
+        }
+        else
+        {
+            System.out.println("Str1 is not equals to str2");
+        }
+    }
+}
